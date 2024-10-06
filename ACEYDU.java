1a76b9d792799b280822ee305fbabc142315816a
Justin Lavoie <113055699+Justin-Lavoie16@users.noreply.github.com>
2024-10-06 17:34:19 +0000

    fixed code

--- ACEYDU.java
+++ ACEYDU.java
@@ -1,100 +1,48 @@
-import java.util.Random;
 import java.util.Scanner;
+import java.util.Random;
 
-public class ACEYDU {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		Random random = new Random();
-		int totalMoney = 100;
-		int betAmount, card1, card2, nextCard;
-		String playAgain;
+public class TimeSpeedDistanceExercise {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        String anotherProblem;
 
-		System.out.println("ACEY-DUCEY IS PLAYED IN THE FOLLOWING MANNER:");
-		System.out.println("THE DEALER (COMPUTER) DEALS TWO CARDS FACE UP.");
-		System.out.println("YOU HAVE THE OPTION TO BET OR NOT TO BET DEPENDING");
-		System.out.println("ON WHETHER OR NOT YOU FEEL THE NEXT CARD WILL HAVE");
-		System.out.println("A VALUE BETWEEN THE FIRST TWO.");
-		System.out.println("IF YOU DO NOT WANT TO BET, INPUT A 0.");
-		System.out.println();
+        do {
+            System.out.println("TIME - SPEED - DISTANCE EXERCISE");
 
-		while (true) {
-			System.out.println("YOU NOW HAVE " + totalMoney + " DOLLARS.");
-			System.out.println();
-			System.out.println("HERE ARE YOUR NEXT TWO CARDS...");
+            // Generate random values
+            int C = random.nextInt(25) + 40; // Speed of car (C)
+            int D = random.nextInt(15) + 5;   // Difference in hours (D)
+            int T = random.nextInt(19) + 20;  // Speed of train (T)
 
-			card1 = random.nextInt(13) + 2;
-			card2 = random.nextInt(13) + 2;
+            // Display the problem
+            System.out.printf("A CAR TRAVELING %d MPH CAN MAKE A CERTAIN TRIP IN\n", C);
+            System.out.printf("%d HOURS LESS THAN A TRAIN TRAVELING AT %d MPH.\n", D, T);
+            System.out.print("HOW LONG DOES THE TRIP TAKE BY CAR: ");
 
-			while (card1 >= card2) {
-				card1 = random.nextInt(13) + 2;
-				card2 = random.nextInt(13) + 2;
-			}
+            // User input
+            double A = scanner.nextDouble();
 
-			printCard(card1);
-			printCard(card2);
+            // Calculate the correct answer
+            double V = (double) D * T / (C - T);
+            int E = (int) Math.round(Math.abs((V - A) * 100 / A));
 
-			System.out.println();
-			System.out.print("WHAT IS YOUR BET: ");
-			betAmount = scanner.nextInt();
+            // Check the answer and provide feedback
+            if (E > 5) {
+                System.out.printf("SORRY. YOU WERE OFF BY %d PERCENT.\n", E);
+            } else {
+                System.out.printf("GOOD! ANSWER WITHIN %d PERCENT.\n", E);
+            }
+            System.out.printf("CORRECT ANSWER IS %.2f HOURS.\n", V);
+            System.out.println();
 
-			if (betAmount == 0) {
-				System.out.println("CHICKEN!!");
-				System.out.println();
-				continue;
-			}
+            // Ask if the user wants another problem
+            System.out.print("ANOTHER PROBLEM (YES OR NO): ");
+            anotherProblem = scanner.next();
+            System.out.println();
 
-			if (betAmount > totalMoney) {
-				System.out.println("SORRY, MY FRIEND, BUT YOU BET TOO MUCH");
-				System.out.println("YOU HAVE ONLY " + totalMoney + " DOLLARS TO BET.");
-				continue;
-			}
+        } while (anotherProblem.equalsIgnoreCase("YES"));
 
-			nextCard = random.nextInt(13) + 2;
-			printCard(nextCard);
-
-			if (nextCard > card1 && nextCard < card2) {
-				System.out.println("YOU WIN!!!");
-				totalMoney += betAmount;
-			} else {
-				System.out.println("SORRY, YOU LOSE.");
-				totalMoney -= betAmount;
-			}
-
-			if (totalMoney <= 0) {
-				System.out.println();
-				System.out.println("SORRY, FRIEND, BUT YOU BLEW YOUR WAD.");
-				System.out.print("TRY AGAIN (YES OR NO): ");
-				playAgain = scanner.next();
-				if (playAgain.equalsIgnoreCase("YES")) {
-					totalMoney = 100;
-				} else {
-					System.out.println();
-					System.out.println("O. K. HOPE YOU HAD FUN!!");
-					break;
-				}
-			}
-		}
-
-		scanner.close();
-	}
-
-	private static void printCard(int card) {
-		switch (card) {
-			case 11:
-				System.out.println("JACK");
-				break;
-			case 12:
-				System.out.println("QUEEN");
-				break;
-			case 13:
-				System.out.println("KING");
-				break;
-			case 14:
-				System.out.println("ACE");
-				break;
-			default:
-				System.out.println(card);
-				break;
-		}
-	}
+        scanner.close();
+    }
 }
